fe8979f1cb989e7a82a38189eea2869d23ab015c
Shumeng <60369045+HonestBook@users.noreply.github.com>
2022-11-19 19:48:43 +0000

    Fix printing result

--- london-tube.py
+++ london-tube.py
@@ -141,7 +141,7 @@ def execute_sql_command_with_markers(command, argument):
 
 # Convert query result, which is a list of tuples, to a list of strings
 def flatten_result(result):
-    return [item for row in result for item in row]
+    return [row[0] for row in result]
 
 def get_station_info(station_name):
     station_query = """
